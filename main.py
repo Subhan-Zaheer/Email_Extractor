503d7c8e84f6e3b3861a09467ddace590da61f1f
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-21 00:38:53 +0500

    Phone number extracter added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ This code will extract emails from given string.
 """
 import re
 str = """
-subhanzaheer2003@gmail.com
+subhanzaheer@gmail.com
 Subhan 
 hassaan@gmail.com
 """
@@ -15,4 +15,16 @@ for pat in pattern1:
     print(pat)
 matches = pattern.finditer(str)
 for match in matches:
+    print(match)
+
+str2 = """
+Subhan contact number is : 0323 1229935
+Ahmad contact number is : 0325-0491734
+Hassaan contact number is : +92 326 4132946
+Azeem chachu number is : 0300 4300176
+Mustanser chachu number is : 03224590922
+"""
+re_for_num = re.compile(r"03[0-4][0-9]\s[0-9]{7}|"+"92\s3[0-4][0-9]\s[0-9]{7}|03[0-4][0-9]-[0-9]{7}|03[0-4][0-9]{8}")
+matches1 = re_for_num.finditer(str2)
+for match in matches1:
     print(match)
