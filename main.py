dae25d47f78428a66d076682c3cff90e70824cd9
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-21 00:02:12 +0500

    Code is done

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 """
 This code will extract emails from given string.
 """
+import re
+str = """
+subhanzaheer2003@gmail.com
+Subhan 
+hassaan@gmail.com
+"""
+pattern = re.compile(r"\w+@\S\w+[.\w]+")
+pattern1 = re.findall(r"[a-z0-9A-Z._%]+@[a-z0-9A-Z_]+[.][a-zA-Z0-9.]+", str)
+pattern2 = re.findall(r"\w+@\S\w+[.\w]+", str)
+print(pattern1)
+for pat in pattern1:
+    print(pat)
+matches = pattern.finditer(str)
+for match in matches:
+    print(match)
